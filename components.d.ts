27d9bba7161de75db02f8afd3ae056c4342b84ef
Huang Chuan <river1440>
2024-11-26 10:01:05 +0800

    feat: update yarn to v4

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,8 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     Button: typeof import('primevue/button')['default']
+    CommonHeader: typeof import('./src/components/common/CommonHeader.vue')['default']
+    Header: typeof import('./src/components/common/Header.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
