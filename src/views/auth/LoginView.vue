356e8d4c5a387d9ffd95b731d7525cb1db39145a
Huang Chuan <river1440>
2024-11-28 11:24:24 +0800

    feat: add title for page setting

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -2,7 +2,28 @@
 </script>
 
 <template>
-  <h1>This is LoginView</h1>
+  <main class="flex flex-row justify-center py-4 bg-stone-50">
+    <div
+      class="fiex flex-col justify-between gap-y-2 w-[516px] border-yellow-300 border-2 bg-yellow-50 p-6 m-8 rounded">
+      <div>input A</div>
+      <div>input B</div>
+      <div>buttom A</div>
+    </div>
+    <!-- <div class="flex flex-col gap-y-6">
+      <section class="max-w-6xl mt-4">
+        <HomeViewBanner />
+      </section>
+      <section class="max-w-6xl mt-4">
+        <BaseLineChart v-model:data="chartData" />
+      </section>
+      <section class="border-yellow-300 border-2 bg-yellow-50 p-6 m-8 rounded flex flex-col w-[1024px] gap-6">
+        <HomePageForm v-model:formInput="formInput" />
+        <div class="flex flex-row justify-center">
+          <Button @click="submit" label="送出" class="text-title" />
+        </div>
+      </section>
+    </div> -->
+  </main>
 </template>
 
 <style scoped></style>
